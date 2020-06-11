0a1677685c5448a1864b78819887b11e32bcdea5
lukehayes <lukepurplehayes@gmail.com>
2020-06-11 15:47:26 +0100

    Added comment header to inherit.js

--- inherit.js
+++ inherit.js
@@ -1,15 +1,24 @@
 'use strict';
 
-function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; subClass.__proto__ = superClass; }
+/**
+ * A simple example of inheritance in 'old style' Javascript
+ */
+
+function inherit(subClass, superClass) {
+    subClass.prototype = Object.create(superClass.prototype);
+    subClass.prototype.constructor = subClass;
+    subClass.__proto__ = superClass;
+}
 
 var BasePerson = function BasePerson() {};
 
-var Person = /*#__PURE__*/function (_BasePerson) {
-  _inheritsLoose(Person, _BasePerson);
+var Person = function (_BasePerson) {
+  inherit(Person, _BasePerson);
 
   function Person() {
     return _BasePerson.call(this) || this;
   }
 
   return Person;
+
 }(BasePerson);
