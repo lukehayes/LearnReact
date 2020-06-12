f5d84c4bb5b84bbab4c16afd3c6fbe8b360bf983
lukehayes <lukepurplehayes@gmail.com>
2020-06-12 17:53:20 +0100

    Added simple-component.js

---
+++ simple-component.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const dom = document.getelementbyid('block_container');
+
+console.debug(dom);
+
+reactdom.render(
+  <h1>hello, world!</h1>,
+  document.getelementbyid('block_container');
+)
