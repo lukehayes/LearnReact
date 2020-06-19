2287fd1350894bc1417c7820d659aaf89435ad76
lukehayes <lukepurplehayes@gmail.com>
2020-06-19 17:46:24 +0100

    Updated simple-component.js with LearnReact tutorial content

--- simple-component.js
+++ simple-component.js
@@ -1,8 +1,34 @@
 'use strict';
 
-const dom = document.getElementById('simple-compontent);
+const DOM = document.getElementById('simple-component');
+
+function tick() {
+    ReactDOM.render(
+        <h1>hello, {new Date().toLocaleTimeString()} !</h1>,
+        DOM
+    );
+}
+
+function Welcome(props) {
+    return <h1> Hello {props.name}</h1>;
+}
+
+function App() {
+
+    return (
+        <div>
+            <Welcome name="Tom"/>
+            <Welcome name="Dick"/>
+            <Welcome name="Harry"/>
+        </div>
+    );
+
+}
+
+let element = <Welcome name="John"/>;
 
 ReactDOM.render(
-  <h1>hello, world!</h1>,
-  document.getElementById('simple-compontent)
-)
+    <App />,
+    DOM
+);
+
