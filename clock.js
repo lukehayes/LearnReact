d234ec44fcc5a4380baea3c97c43526516a34476
lukehayes <lukepurplehayes@gmail.com>
2020-06-23 17:05:00 +0100

    Implemented basic, working Clock component class

--- clock.js
+++ clock.js
@@ -2,19 +2,45 @@
 
 class Clock extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {date: new Date()}
+    }
+
+    componentDidMount() {
+
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentDidUnMount() {
+
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+
+        this.setState({
+            date: new Date()
+        });
+    }
+
+
     render() {
         return(
             <React.Fragment>
                 <h3>Clock Component</h3>
-                <h4>It is {this.props.date.toLocaleTimeString()}</h4>
+                <h4>It is {this.state.date.toLocaleTimeString()}</h4>
             </React.Fragment>
         );
     }
 
 }
 
-
 ReactDOM.render(
-    <Clock date={new Date()}/>,
+    <Clock />,
     document.getElementById('clock')
 );
