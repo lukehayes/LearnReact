7a3da68ac9272c07e5aa7b28b12855cd240592ec
lukehayes <lukepurplehayes@gmail.com>
2020-06-23 15:11:24 +0100

    Clock component now using ES6 classes instead of functions

--- clock.js
+++ clock.js
@@ -1,16 +1,20 @@
 'use strict';
 
+class Clock extends React.Component {
 
-function Clock(props) {
-    return(
-        <React.Fragment>
-            <h3>Clock Component</h3>
-            <h4>It is {props.date.toLocaleTimeString()}</h4>
-        </React.Fragment>
-    );
+    render() {
+        return(
+            <React.Fragment>
+                <h3>Clock Component</h3>
+                <h4>It is {this.props.date.toLocaleTimeString()}</h4>
+            </React.Fragment>
+        );
+    }
+
 }
+
 
 ReactDOM.render(
     <Clock date={new Date()}/>,
     document.getElementById('clock')
-)
+);
